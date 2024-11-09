e005c05373ae9e8f836659e7d436529169217cca
Vinicius Calebe Barbosa de Moura <viniciuscalebebarbosa@gmail.com>
2024-11-09 02:01:45 -0300

    vasmos testar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, Form, UploadFile, File
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
